195608e07cd17d9a8589d6e55e835782cd961fcd
elaman <elaman.bazarov98@gmail.com>
2017-05-28 19:26:33 +0600

    some details added

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MAIN_PATH: __dirname,
-  TELEGRAM_TOKEN: process.env.TOKEN || 'my_test_token',
+  TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN || 'my_test_token',
   SERVER_URL: process.env.SERVER_URL || 'my_url_there'
 }
--- routes/telegram.js
+++ routes/telegram.js
@@ -1,12 +1,14 @@
 var express = require('express');
-var bot = require('../telegram-bot')
+var bot = require('../telegram-bot');
 var router = express.Router();
 
 module.exports = function(app) {
 	var db = app.get('db');
 	var config = app.get('config');
   router.post('/', function(req, res) {
-
+  	console.log(req.body);
+  	bot.processUpdate(req.body);
+  	res.end();
   })
 
   app.use('/telegram', router);
