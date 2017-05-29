4f027d17343b55ad9ed58b46a2480c09c48a4691
elaman <elaman.bazarov98@gmail.com>
2017-05-29 16:37:59 +0600

    done

---
+++ answer.js
@@ -0,0 +1,5 @@
+module.exports = {
+	"welcome_text": {
+		"ru": "Здарова"
+	}
+}
--- telegram-bot.js
+++ telegram-bot.js
@@ -2,13 +2,18 @@ var TelegramBot = require('node-telegram-bot-api');
 var config = require(__dirname + '/config.js');
 var url = config.SERVER_URL;
 var token = config.TELEGRAM_TOKEN;
+var answer = require(config.MAIN_PATH + '/answer.js');
 var bot = new TelegramBot(token);
-console.log(url);
+
 bot.setWebHook(`${url}/telegram`);
 
-bot.on('message', msg => {
+bot.on('message', function(msg) {
   bot.sendMessage(msg.chat.id, 'Да, я с детства в аду, стресс, я тону....');
 });
+
+bot.onText('/\/start/', function(msg) {
+	bot.sendMessage(msg.chat.id, answer.welcome_text['ru']);
+})
 
 
 module.exports = bot;
